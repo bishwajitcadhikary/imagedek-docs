4f50b7f97cd8fe0f4c5c0d6c5511539452de164f
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2023-03-12 17:17:21 +0600

    Update

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,10 +1,10 @@
 
 export default {
     title: 'AI Image',
-    logo: <span>AI Image - AI Image Generator Assistant (SAAS)</span>,
+    logo: <span>ImageAI - ImageAI Generator Assistant (SAAS)</span>,
     footer: {
         text: <span>
-      Copyright © {new Date().getFullYear()} <a href="https://codecanyon.net/user/abnDevs" target="_blank">abnDevs</a>. All Rights Reserved.
+      Copyright © {new Date().getFullYear()} <a href="https://codecanyon.net/user/abndevs" target="_blank">abnDevs</a>. All Rights Reserved.
     </span>,
     }
 }
