ee7c118bdcb51ac7836a16fbf085f8bea27a3e90
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2023-03-13 05:21:03 +0600

    Bug Fixed

--- next.config.js
+++ next.config.js
@@ -1,23 +1,13 @@
-/** @type {import('next').NextConfig} */
-const withNextra = require("nextra")({
-    theme: "nextra-theme-docs",
-    themeConfig: "./theme.config.jsx",
-});
+const withNextra = require('nextra')({
+    theme: 'nextra-theme-docs',
+    themeConfig: './theme.config.jsx',
+})
 
-const isProduction = process.env.NODE_ENV === "production";
-const assetPrefix = isProduction ? "" : "";
-
-const nextConfig = {
+module.exports = withNextra({
     images: {
         unoptimized: true,
     },
     reactStrictMode: true,
     swcMinify: true,
     trailingSlash: true,
-    assetPrefix,
-    basePath: assetPrefix,
-};
-
-module.exports = withNextra({
-    ...nextConfig,
-});
+})
--- theme.config.jsx
+++ theme.config.jsx
@@ -1,10 +1,21 @@
-
 export default {
-    title: 'AI Image',
-    logo: <span>ImageAI - ImageAI Generator Assistant (SAAS)</span>,
+    logo: <span>ImageAI - AI Image Generator Assistant (SAAS)</span>,
+    useNextSeoProps() {
+        return {
+            titleTemplate: '%s – ImageAI'
+        }
+    },
+    head: (
+        <>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta property="og:title" content="ImageAI - AI Image Generator Assistant (SAAS)" />
+            <meta property="og:description" content="ImageAI is the ultimate AI image generator assistant for designers, marketers, and content creators. With its cutting-edge technology and intuitive interface, ImageAI helps you create stunning visuals that are sure to captivate your audience." />
+        </>
+    ),
     footer: {
         text: <span>
-      Copyright © {new Date().getFullYear()} <a href="https://codecanyon.net/user/abndevs" target="_blank">abnDevs</a>. All Rights Reserved.
-    </span>,
-    }
+            Copyright © {new Date().getFullYear()} <a href="https://codecanyon.net/user/abndevs" target="_blank">abnDevs</a>. All Rights Reserved.
+        </span>,
+    },
+
 }
