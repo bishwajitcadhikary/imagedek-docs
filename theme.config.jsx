b16c35743634cf4a280e79f44a42fc6cc980996e
Bishwajit Adhikary <bishwajitcadhikary@gmail.com>
2025-05-07 14:51:38 +0600

    Refactor documentation structure and update content for clarity

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,7 @@
 export default {
     logo: <>
-        <img src="https://www.linkpicture.com/q/logo-docs.png" alt="logo" width={200}/>
+        <img src="/images/logo-dark.png" alt="logo" className="logo-light"/>
+        <img src="/images/logo.png" alt="logo" className="logo-dark"/>
     </>,
     useNextSeoProps() {
         return {
@@ -10,10 +11,19 @@ export default {
     project: {
         icon: (
             <>
-                <img src="https://www.linkpicture.com/q/codecanyon-logo.png" alt="logo" width={50} />
+                <img src="/images/wrap-bootstrap.png" alt="logo" width={50} />
             </>
         ),
-        link: 'https://codecanyon.net/item/imagedek-ai-image-generator-assistant-saas/44438866'
+        link: 'https://wrapbootstrap.com/user/kindigi',
+    },
+    chat: {
+        icon: (
+            <>
+                <img src="/images/kindigi-light.png" alt="chat" width={50} className={"icon-dark"} />
+                <img src="/images/kindigi-dark.png" alt="chat" width={50} className={"icon-light"} />
+            </>
+        ),
+        link: 'https://kindigi.com',
     },
     docsRepositoryBase: 'https://github.com/bishwajitcadhikary/imagedek-docs/tree/main/',
     head: (
@@ -22,23 +32,15 @@ export default {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta property="og:title" content="ImageDek - AI Image Generator Assistant (SAAS)" />
             <meta property="og:description" content="ImageDek is the ultimate AI image generator assistant for designers, marketers, and content creators. With its cutting-edge technology and intuitive interface, ImageDek helps you create stunning visuals that are sure to captivate your audience." />
-            <meta property="og:image" content="https://www.linkpicture.com/q/imagedek.png" />
-            <meta property="og:url" content="https://docs.imagedek.com/" />
+            <meta property="og:image" content="https://imagedek-docs.vercel.app/images/preview.png" />
+            <meta property="og:url" content="https://imagedek.kindigi.com/" />
             <meta property="og:type" content="website" />
             <meta property="fb:app_id" content="234635452287600" />
-
-            <meta name="twitter:card" content="ImageDek is the ultimate AI image generator assistant for designers, marketers, and content creators. With its cutting-edge technology and intuitive interface, ImageDek helps you create stunning visuals that are sure to captivate your audience." />
-            <meta name="twitter:site" content="@nazrul0132" />
-            <meta name="twitter:creator" content="@nazrul0132" />
-            <meta name="twitter:title" content="ImageDek - AI Image Generator Assistant (SAAS)" />
-            <meta name="twitter:description" content="ImageDek is the ultimate AI image generator assistant for designers, marketers, and content creators. With its cutting-edge technology and intuitive interface, ImageDek helps you create stunning visuals that are sure to captivate your audience." />
-            <meta name="twitter:image" content="https://www.linkpicture.com/q/imagedek.png" />
         </>
     ),
     footer: {
         text: <span>
-            Copyright © {new Date().getFullYear()} <a href="https://codecanyon.net/user/designing-world" target="_blank">designing-world</a>. All Rights Reserved.
+            Copyright © {new Date().getFullYear()} <a href="https://kindigi.com" target="_blank">KinDigi</a>. All Rights Reserved.
         </span>,
     },
-
 }
